3a60c3a057ec1829566184f13f728d1624cb212e
Clement <clementmaganza@gmail.com>
2025-01-07 08:55:03 +0100

    creation de toutes les view

--- mnt/private/routes/web.php
+++ mnt/private/routes/web.php
@@ -14,5 +14,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Base');
 });
+
+Route::get('/connexion',function(){
+    return view('ConnexionPage');
+})->name('connexion');
+
+Route::get('/inscription',function(){
+    return view('SignInForm');
+})->name('inscription');
+
+Route::get('/profile',function(){
+    return view('MyProfile');
+})->name('profile');
+
+Route::get('/session',function(){
+    return view('SessionsPage');
+})->name('session');
+
+Route::get('/session/create',function(){
+    return view('SessionCreate');
+})->name('session.create');
+
+Route::get('/session/create/form',function(){
+    return view('SessionCreateForm');
+})->name('session.create.form');
+
+Route::get('/modifing', function(){
+    return view('SiteModifing');
+});
+
+Route::get('/students', function(){
+    return view('StudentsSheet');
+});
+
+Route::get('/user', function(){
+    return view('UserModifing');
+});
+
+Route::get('/validate', function(){
+    return view('ValidateLevel');
+});
+
+Route::get('/sheet', function(){
+    return view('EvolutiveSheet');
+});
+
