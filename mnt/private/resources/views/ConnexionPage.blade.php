9119a7ea56042479e4f7e2935266a15855ff551a
Louisa <Louisa@non.fr>
2025-01-07 14:32:39 +0100

    Adding a background image

--- mnt/private/resources/views/ConnexionPage.blade.php
+++ mnt/private/resources/views/ConnexionPage.blade.php
@@ -1,7 +1,24 @@
-@extends('Base')
+<!doctype html>
+<html lang="fr">
 
-@section('title','a définir')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="/css/app.css" rel="stylesheet">
 
-@section('content')
-    <!-- vous bossez la dedans -!-->
-@endsection
+    <title>Secoule : Connexion</title>
+</head>
+
+<body class="turtle-background">
+    <h1> Se connecter </h1>
+        <div class="card center" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Connexion</h5>
+                <p class="card-text"></p>
+                <a href="#" class="btn btn-primary">Se connecter</a>
+            </div>
+        </div>
+</body>
+
+</html>
