bcfb62639ac6f725f89416d42abe22366e27bf07
Victor <victoryvray@gmail.com>
2025-01-08 12:22:44 +0100

    Page des séances fonctionnelle

--- mnt/private/app/Http/Controllers/SessionController.php
+++ mnt/private/app/Http/Controllers/SessionController.php
@@ -7,8 +7,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use function GuzzleHttp\json_decode;
 
+
 class SessionController extends Controller
 {
+
+
     public function show(){
 
 
@@ -20,51 +23,30 @@ class SessionController extends Controller
             ->select('grp2_club.club_name')
             ->first();
 
-        $seance = DB::table('grp2_user')
+
+        $sessions = DB::table('grp2_user')
             ->join('grp2_attendee', 'grp2_user.user_id', '=', 'grp2_attendee.user_id')
-            ->join('attend','attend.atte_id','=','attend.atte_id')
-            ->join('grp2_session','attend.sess_id','=','grp2_session.sess_id')
-            ->join('grp2_evaluation', 'grp2_evaluation.sess_id', '=', 'grp2_session.sess_id')
-            ->join('grp2_ability', 'grp2_evaluation.abi_id', '=', 'grp2_ability.abi_id')
-            ->join('grp2_skill', 'grp2_skill.skill_id', '=', 'grp2_ability.skill_id')
+            ->join('grp2_session', 'grp2_attendee.sess_id', '=', 'grp2_session.sess_id')
+            ->join('grp2_evaluation', 'grp2_session.sess_id', '=' ,'grp2_evaluation.sess_id')
+            ->join('grp2_ability','grp2_ability.abi_id','=','grp2_evaluation.abi_id')
+            ->join('grp2_skill','grp2_ability.skill_id','=', 'grp2_skill.skill_id')
             ->where('grp2_user.user_id', '=', session('user_id'))
-            ->select('grp2_user.*', 'attend.*','grp2_session.*','grp2_ability.*','grp2_evaluation.*','grp2_skill.*')
             ->get();
 
-        $session = DB::table('grp2_user')
-            ->join('grp2_attendee', 'grp2_user.user_id', '=', 'grp2_attendee.user_id')
-            ->join('attend','attend.atte_id','=','attend.atte_id')
-            ->join('grp2_session','attend.sess_id','=','grp2_session.sess_id')
-            ->select(DB::raw('count(*) as nb'))
-            ->get();
-
-        $abilities =  DB::table('grp2_user')
-            ->join('grp2_attendee', 'grp2_user.user_id', '=', 'grp2_attendee.user_id')
-            ->join('attend','attend.atte_id','=','attend.atte_id')
-            ->join('grp2_session','attend.sess_id','=','grp2_session.sess_id')
-            ->join('grp2_evaluation', 'grp2_evaluation.sess_id', '=', 'grp2_session.sess_id')
-            ->join('grp2_ability', 'grp2_evaluation.abi_id', '=', 'grp2_ability.abi_id')
-            ->join('grp2_skill', 'grp2_skill.skill_id', '=', 'grp2_ability.skill_id')
-            ->where('grp2_user.user_id', '=', session('user_id'))
-            ->select('grp2_ability.*')
+        $abilities = DB::table('grp2_ability')
+            ->join('grp2_evaluation','grp2_ability.abi_id','=','grp2_evaluation.abi_id')
+            ->join('grp2_session','grp2_evaluation.sess_id','=','grp2_session.sess_id')
             ->get();
 
 
 
 
-            /*
-            ->join('grp2_club', 'grp2_club.club_id', '=', 'report.club_id')
-            ->where('grp2_club.club_id', session('user_id'))
-            ->select('grp2_user.user_id')
-            ->get();*/
 
 
 
 
-        //session('user_id')
+        return view('SessionsPage',['club'=>$club, 'sessions'=>$sessions, 'abilities'=>$abilities]);
 
-
-        return view('SessionsPage',['club'=>$club,'seance'=>$seance,'abilities'=>$abilities, 'session'=>$session]);
 
         //Ici, il faut récupérer les éléments des models avec les select puis les mettre dans des variables
         //Puis afficher la vue en passant les variables en tableau comme grafikart (Moteur template Blade 8:57 mins).
--- mnt/private/resources/views/SessionsPage.blade.php
+++ mnt/private/resources/views/SessionsPage.blade.php
@@ -1,17 +1,20 @@
 @extends('Base')
-
 @section('title','Page des séances')
-
 @section('content')
+
+    {{session(['verify' => '0'])}}
+
     @if(session('user_mail'))
-        <p>bonjour {{ session('user_mail')  }} {{ session('user_id') }}</p>
+        <p>bonjour {{ session('user_mail')  }}</p>
     @else
         <p>Aucun utilisateur connecté.</p>
     @endif
 
+
     <div class="paragraphe-bienvenue">
         <p>
             <strong>Bienvenue dans le club : {{$club->club_name}} </strong>
+            <br>
             Sed mattis, erat sit amet gravida malesuada, elit augue egestas diam, tempus scelerisque nunc nisl vitae libero. Sed consequat feugiat massa. Nunc porta, eros in eleifend varius, erat leo rutrum dui, non convallis lectus orci ut nibh. Sed lorem massa, nonummy quis, egestas id, condimentum at, nisl. Maecenas at nibh. Aliquam et augue at nunc pellentesque ullamcorper. Duis nisl nibh, laoreet suscipit, convallis ut, rutrum id, enim. Phasellus odio. Nulla nulla elit, molestie non, scelerisque at, vestibulum eu, nulla. Ut odio nisl, facilisis id, mollis et, scelerisque nec, enim. Aenean sem leo, pellentesque sit amet, scelerisque sit amet, vehicula pellentesque, sapien.
         </p>
     </div>
@@ -19,23 +22,30 @@
     <div class="seances">
 
         <h2> Mes séances : </h2>
-        @for($i = 0; $i < $session[0]->nb; $i++)
-            <div class="seance">
-                <ul>
-                    <li>Session Date: {{$seance[0]->SESSION_DATE}}</li>
-                    <li>Niveau: {{$seance[0]->LEVEL_ID}}</li>
-                    <li>Skill C{{$seance[0]->SKILL_ID}}: {{$seance[0]->SKILL_LABEL}}</li>
-                </ul>
-                <div class="abilities">
-                    <h4>Abilities:</h4>
+        @foreach($sessions as $session)
+            @if($session->SESS_ID != session('verify'))
+                <p>-------------------------------------------------------------</p>
+                {{session(['verify' => $session->SESS_ID])}}
+                <div class="seance">
                     <ul>
-                        @foreach($abilities as $ability)
-                            <li>A{{$ability->ABI_ID}}: {{$ability->ABI_LABEL}}</li>
-                        @endforeach
+                        <li>Session Date: {{$session->SESS_DATE}}</li>
+                        <li>Niveau: {{$session->LEVEL_ID}} </li>
+                        <li>Skill C: {{$session->SKILL_LABEL}} </li>
                     </ul>
+                    <div class="abilities">
+
+                        <h4>Abilities: </h4>
+                        <ul>
+                            @foreach($abilities as $ability )
+                            <li>
+                                    {{$ability->ABI_LABEL}}
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        @endfor
+            @endif
+        @endforeach
 
     </div>
 
