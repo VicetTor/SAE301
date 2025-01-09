8d77c74caa05852ae5dfd267fa700dcbe2344140
TOUKAM <houllegatte.tom@gmail.com>
2025-01-09 23:01:30 +0100

    Error modification FormsTrainingController

--- mnt/private/app/Http/Controllers/FormsTrainingController.php
+++ mnt/private/app/Http/Controllers/FormsTrainingController.php
@@ -144,4 +144,17 @@ class FormsTrainingController extends Controller {
         // Retour avec succès
         return view('TrainingHome');
     }
+
+    public function showModificationTechnical() {
+        $abilities = DB::table('grp2_ability')
+        ->get();
+        return view('TrainingModificationTechnical', ['abilities'=>$abilities]);
+    }
+    public function UpdateAbilities(Request $request) {
+        DB::table('grp2_ability')
+            ->where('abi_id', '=', $request->abilitie_id)
+            ->update(['abi_label' => $request->new_abilitie_id]);
+        // Retour avec succès
+        return view('TrainingHome');
+    }
 }
