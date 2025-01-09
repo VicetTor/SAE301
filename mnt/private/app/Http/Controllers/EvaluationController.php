acf6efb647dd51412eee9b6300aeb4dc243a6747
VicetTor <victoryvray@gmail.com>
2025-01-10 00:47:51 +0100

    Add FIX

--- mnt/private/app/Http/Controllers/EvaluationController.php
+++ mnt/private/app/Http/Controllers/EvaluationController.php
@@ -43,7 +43,7 @@ class EvaluationController extends Controller
      * )
      */
     public function historiqueEvaluations($userId, $clubId) {
-        if (session('type_id') != 4) {
+        if (session('type_id') != 4 && session('type_id') != 3) {
             return redirect()->route('home');
         }
         if (session('user_id') == null) {
@@ -52,12 +52,12 @@ class EvaluationController extends Controller
         // Retrieve the user and the club by their IDs
         $user = User::find($userId);
         $club = Club::find($clubId);
-    
+
         // Check if the user and the club exist
         if (!$user || !$club) {
             return response()->json(['message' => 'User or club not found.'], 404);
         }
-    
+
         // Retrieve the evaluations for the specified user and club
         $evaluations = Evaluation::with(['user', 'validation.level', 'validation.skill'])
             ->where('USER_ID', $userId)
@@ -66,12 +66,12 @@ class EvaluationController extends Controller
                 $query->where('CLUB_ID', $clubId);
             })
             ->get();
-    
+
         // If no evaluations are found, return a 404 response
         if ($evaluations->isEmpty()) {
             return response()->json(['message' => 'No evaluations found for this user in this club.'], 404);
         }
-    
+
         // Check if the current session user has editing privileges
         $canEdit = session('type_id') == 4 || session('type_id') == 3;
 
@@ -111,22 +111,22 @@ class EvaluationController extends Controller
      */
     public function search(Request $request) {
 
-        if (session('type_id') != 4) {
+        if (session('type_id') != 4 && session('type_id') != 3) {
             return redirect()->route('home');
         }
         if (session('user_id') == null) {
             return redirect()->route('connexion');
         }
-        
+
         // Retrieve the search term and optional user/club filters from the request
         $searchTerm = $request->input('search');
         $userId = $request->input('userId');
-        
+
         $club =DB::table('report')
         ->where('report.user_id' , '=', Session('user_id'))
-        ->first();  
-        $clubId = $club->CLUB_ID;    
-    
+        ->first();
+        $clubId = $club->CLUB_ID;
+
         // Build the query to filter evaluations
         $evaluationsQuery = Evaluation::query()
             ->with(['user', 'validation.level', 'validation.skill', 'user.reports']) // Load related data
@@ -150,19 +150,19 @@ class EvaluationController extends Controller
                     $query->where('SKILL_LABEL', 'like', '%' . $searchTerm . '%');
                 });
             });
-    
+
         // Apply user ID filter if provided
         if ($userId) {
             $evaluationsQuery->where('USER_ID', $userId);
         }
-    
+
         // Apply club ID filter if provided
         if ($clubId) {
             $evaluationsQuery->whereHas('user.reports', function ($query) use ($clubId) {
                 $query->where('CLUB_ID', $clubId);
             });
         }
-    
+
         // Execute the query and retrieve the results
         $evaluations = $evaluationsQuery->get();
 
@@ -208,11 +208,11 @@ class EvaluationController extends Controller
         $user_id = $request->input('user_id');
         $abi_id = $request->input('abi_id');
         $sess_id = $request->input('sess_id');
-    
+
         if($evalId == 0){
             $evalId = Evaluation::count() + 1;
         }
-    
+
         DB::table('grp2_evaluation')->updateOrInsert(
             ['EVAL_ID' => $evalId],
             [
@@ -222,7 +222,7 @@ class EvaluationController extends Controller
                 'ABI_ID' => $abi_id
             ]
         );
-    
+
         return response()->json(['message' => 'Nouvelle évaluation créée avec succès!']);
     }
 }
