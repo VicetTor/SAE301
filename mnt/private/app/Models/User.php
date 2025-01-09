a5993d5e4111cc0bea4ca30e44f355bcf1a3a0ed
Cassandre <cassandre.yontailleux@gmail.com>
2025-01-09 08:38:15 +0100

    delete import

--- mnt/private/app/Models/User.php
+++ mnt/private/app/Models/User.php
@@ -4,10 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use OpenApi\Annotations as OA;
-use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
