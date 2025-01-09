da640b6f0f8556433aeb0521cd1fc3066659c678
Cassandre <cassandre.yontailleux@gmail.com>
2025-01-09 10:25:03 +0100

    Display of SessionPage is working !

--- mnt/private/resources/views/SessionsPage.blade.php
+++ mnt/private/resources/views/SessionsPage.blade.php
@@ -9,7 +9,7 @@
         <h3> <strong> Mes Séances : </strong> </h3>
 
         <br>
-        <div id="sessionsCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div id="sessionsCarousel" class="carousel carousel-dark slide " data-bs-ride="carousel">
             <!-- Indicateurs (points de navigation en bas) -->
             <div class="carousel-indicators">
                 @foreach($sessions->chunk(4) as $index => $chunk)
@@ -24,27 +24,31 @@
                         <div class="d-flex justify-content-center">
                             @foreach($chunk as $session)
                                 @if($session->SESS_ID != session('verify'))
-                                    <div class="card text-bg-light mb-3 rounded-5 p-3 back flex-column me-3 bg-body-secondary" style="max-width: 18rem;">
+                                    
+                                    <div class="card m-5" style="width: 18rem;">
                                         {{ session(['verify' => $session->SESS_ID]) }}
-                                        <div class="seance">
-                                            <ul>
-                                                <li><strong>Date de session</strong> : <br> {{$session->SESS_DATE}}</li>
-                                            </ul>
-                                            <ul>
-                                                <li style="list-style: none"><strong>Niveau </strong>:&nbsp;{{$session->LEVEL_ID}}</li>
-                                                <br>
-                                                <li style="list-style: none"><strong>Compétence C{{$session->SKILL_ID}}</strong> : <br> {{$session->SKILL_LABEL}}</li>
-                                            </ul>
-                                            <div class="abilities">
-                                                <ul>
-                                                    @foreach($abilities as $ability)
-                                                        @if($ability->SESS_DATE == $session->SESS_DATE)
-                                                            <li style="list-style: none">
-                                                                <span style="color: red"><strong>A{{$ability->ABI_ID}}</strong></span> : {{$ability->ABI_LABEL}}
-                                                            </li>
-                                                        @endif
-                                                    @endforeach
-                                                </ul>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Prochaine Séance</h5>
+                                            <div class="card-text">
+                                                
+                                                <h4 class="fw-bold p-2 fs-4"> {{$session->SESS_DATE}}</h4>
+                                                
+                                                <div class="shadow-sm p-2 bg-body-tertiary rounded">
+                                                    <h4 class="fw-bold p-2 fs-6">Niveau :&nbsp;{{$session->LEVEL_ID}}</h4>
+                                                    <div class="shadow-sm bg-body-secondary rounded">
+                                                        <h4 class="fw-bold p-2 fs-6">Compétence C{{$session->SKILL_ID}} : </h4><p class="p-2 fs-6"> {{$session->SKILL_LABEL}}</p>
+                                                        <div class="shadow-sm p-2 bg-dark-subtle rounded">
+                                                            @foreach($abilities as $ability)
+                                                                @if($ability->SESS_DATE == $session->SESS_DATE)
+                                                                    <div>
+                                                                        <h4 class="fw-bold d-inline p-2 fs-6"> A{{$ability->ABI_ID}} :</h4> <p class="d-inline p-2 fs-6"> {{$ability->ABI_LABEL}}<p>
+                                                                    </div>
+                                                                @endif
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                
                                             </div>
                                         </div>
                                     </div>
@@ -53,32 +57,31 @@
                         </div>
                     </div>
                 @endforeach
-            </div>
 
+            </div>
             <!-- Boutons de navigation -->
             <button class="carousel-control-prev" type="button" data-bs-target="#sessionsCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="carousel-control-prev-icon " aria-hidden="true"></span>
                 <span class="visually-hidden">Précédent</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#sessionsCarousel" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Suivant</span>
             </button>
-
-            <br>
-            <div class="d-flex justify-content-center">
-                <button data-mdb-button-init data-mdb-ripple-init class="col-lg-2 btn btn-primary" type="button" data-dismiss="modal">Voir la progression</button>
-            </div>
         </div>
     </div>
 
-            <script>
+    <script>
         document.addEventListener('DOMContentLoaded', function() {
             var carousel = new bootstrap.Carousel('#sessionsCarousel', {
                 wrap:true // Revenir au premier slide après le dernier
             });
         });
     </script>
+
+    <div class="d-flex justify-content-center">
+        <button data-mdb-button-init data-mdb-ripple-init class="col-lg-2 btn btn-primary" type="button" data-dismiss="modal">Voir la progression</button>
+    </div>
 
 
 @endsection
