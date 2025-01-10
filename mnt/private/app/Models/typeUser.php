d7de7db67ee21b801fa4ec6caf9cf2d268a6f539
Paul8710 <paulletellierpro@gmail.com>
2025-01-10 12:41:00 +0100

    ekzod

--- mnt/private/app/Models/typeUser.php
+++ mnt/private/app/Models/typeUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class typeUser extends Model
+class TypeUser extends Model
 {
     use HasFactory;
     public $timestamps = false;
