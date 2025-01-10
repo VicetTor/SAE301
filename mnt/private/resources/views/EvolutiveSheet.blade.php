b354a460b0d5e3f0bb1bf9a74b60bce9f4a24450
hpottier272 <hugo272@orange.com>
2025-01-10 09:38:14 +0100

    Commantaire evolutiveSheet

--- mnt/private/resources/views/EvolutiveSheet.blade.php
+++ mnt/private/resources/views/EvolutiveSheet.blade.php
@@ -46,23 +46,20 @@ use App\Models\Skill;
 
     <!-- If the user is not logged in (session 'user_mail' is missing), show a "not connected" message -->
     @if(session()->missing('user_mail'))
-        <p> PAS CONNECTE </p> <!-- "Not connected" -->
-    @endif
-
-    <!-- Greet the user with their first and last name from the session -->
-    <p> Bonjour {{ session('user_firstname') }} {{ session('user_lastname') }} </p> <!-- Greeting the user -->
-    
-    <!-- Display the user's level -->
-    <p> Vous etes niveau {{ session('level_id') }} </p> <!-- Display user's level -->
-
-    @if(session()->missing('user_mail'))
         <p> Vous êtes actuellement NON CONNECTÉ </p>
     @endif
 
-    <p> Bonjour {{ session('user_firstname') }} {{ session('user_lastname') }} </p>
-    <p> Votre progression vers le Niveau {{ session('level_id_resume') }}</p>
+    <!-- Greet the user with their first and last name from the session -->
+    <p class="fw-medium fs-3"> Vous êtes connecté(e) en tant que : {{ session('user_firstname') }} {{ session('user_lastname') }} </p>
+
+    <!-- Display the user's level -->
+    <p class="fst-italic fs-5"> Vous etes niveau {{ session('level_id') }} </p> <!-- Display user's level -->
+
+    <!-- Display the user's target level -->
+    <p class="fst-italic fs-5"> Votre progression vers le Niveau {{ session('level_id_resume') }}</p> <!-- Display the user's target level -->
 
     <table>
+        <!-- Displays the table header -->
         <thead>
             <tr>
                 <th>Date</th>
@@ -71,79 +68,77 @@ use App\Models\Skill;
                 <th>Évolution</th>
             </tr> 
         </thead>  
+
         <tbody>
             @php 
                 $i = 0; 
-                $j = 0; 
             @endphp
-            
+            <!-- foreach to browse all sessions --->
             @foreach ($sessions as $session)
-            
-            <?php
-            
-            $skills = DB::select(DB::raw('
-            select distinct GRP2_SKILL.SKILL_ID, GRP2_SKILL.SKILL_LABEL from GRP2_SKILL
-            inner join GRP2_ABILITY using (SKILL_ID)
-            inner join GRP2_EVALUATION using (ABI_ID)
-            where GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
-            and GRP2_SKILL.LEVEL_ID ='.$level.'
-            and GRP2_EVALUATION.USER_ID ='.$user_id
-            ));
-            
-
-
-            $nbSkills = count($skills);
-            $taille = 0;
-            foreach($skills as $skill){
-
-                $result = DB::select(DB::raw('
-                select * from GRP2_ABILITY
-                inner join GRP2_EVALUATION using (ABI_ID)
-                where GRP2_ABILITY.SKILL_ID ='.$skill->SKILL_ID.'
-                and GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
-                and GRP2_EVALUATION.USER_ID ='.$user_id
-                ));
-
-                $taille+=count($result);
-            }
-            ?>
-                <!-- insére les dates -->
-                <td rowspan="{{ $taille }}" class="session-date">
-                    {{ $session->SESS_DATE }}
-                </td>
-                @php $debug = 0; @endphp
-                @foreach ($skills as $skill)
-                    
                 <?php
+                    /* request that allows you to have all the skills worked on during a session */
+                    $skills = DB::select(DB::raw('
+                    select distinct GRP2_SKILL.SKILL_ID, GRP2_SKILL.SKILL_LABEL from GRP2_SKILL
+                    inner join GRP2_ABILITY using (SKILL_ID)
+                    inner join GRP2_EVALUATION using (ABI_ID)
+                    where GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
+                    and GRP2_SKILL.LEVEL_ID ='.$level.'
+                    and GRP2_EVALUATION.USER_ID ='.$user_id
+                    ));
+                    $nbSkills = count($skills);
+                    $taille = 0;
+                    /* foreach to browse all skills */
+                    foreach($skills as $skill){
+                        /* request that allows you to have all the skills you've worked on during a session */
                         $result = DB::select(DB::raw('
                         select * from GRP2_ABILITY
                         inner join GRP2_EVALUATION using (ABI_ID)
-                        where GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
-                        and GRP2_EVALUATION.USER_ID ='.$user_id.'
-                        and GRP2_ABILITY.SKILL_ID ='.$skill->SKILL_ID
+                        where GRP2_ABILITY.SKILL_ID ='.$skill->SKILL_ID.'
+                        and GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
+                        and GRP2_EVALUATION.USER_ID ='.$user_id
                         ));
-                        $nombre = count($result);
+
+                        $taille+=count($result);
+                    }
+                ?>
+                <!-- line that inserts the dates -->
+                <td rowspan="{{ $taille }}" class="session-date">
+                    {{ $session->SESS_DATE }}
+                </td>
+                <!-- foreach to browse all skills --->
+                @foreach ($skills as $skill)
+                    <!-- request that allows you to have all the skills you've worked on during a session --->
+                    <?php
+                            $result = DB::select(DB::raw('
+                            select * from GRP2_ABILITY
+                            inner join GRP2_EVALUATION using (ABI_ID)
+                            where GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
+                            and GRP2_EVALUATION.USER_ID ='.$user_id.'
+                            and GRP2_ABILITY.SKILL_ID ='.$skill->SKILL_ID
+                            ));
+                            $nombre = count($result);
                     ?>
 
-                    <!-- insére les compétences -->
+                    <!-- line that inserts the skills -->
                     <td rowspan="{{$nombre}}" class="skill">
                         {{ $skill->SKILL_LABEL }} {{$nbSkills}}
                     </td>
-
+                        <!-- query that lets you see all the skills worked on during a session --->
                     <?php
-                    $aptitude = DB::select(DB::raw('
-                    select * from GRP2_ABILITY
-                    inner join GRP2_EVALUATION using (ABI_ID)
-                    where SKILL_ID = '.$skill->SKILL_ID.'
-                    and GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
-                    and GRP2_EVALUATION.USER_ID ='.$user_id
+                        $aptitude = DB::select(DB::raw('
+                            select * from GRP2_ABILITY
+                            inner join GRP2_EVALUATION using (ABI_ID)
+                            where SKILL_ID = '.$skill->SKILL_ID.'
+                            and GRP2_EVALUATION.SESS_ID ='.$session->SESS_ID.'
+                            and GRP2_EVALUATION.USER_ID ='.$user_id
 
-                    ));
-                    $compteur = 0;
+                        ));
+                        $compteur = 0;
                     ?>
 
-                    
+                    <!-- foreach to browse all abilitys of the session --->
                     @foreach($aptitude as $apt)
+                        <!-- allows you to test whether there are evaluations that are rated --->
                         @php
                             $evaluationTrouvee = null;
                             foreach($evaluationsChaqueSeance[$i] as $eval) {
@@ -156,11 +151,11 @@ use App\Models\Skill;
                         @if($compteur != 0) 
                             <tr> 
                         @endif
-                        <!-- insére les aptitudes -->
+                        <!-- line for inserting ability -->
                         <td class = "ability">
                             {{$apt->ABI_LABEL}}
                         </td>
-                        <!-- insére les évaluations -->
+                        <!-- line for inserting evaluations -->
                         <td class="eval"> 
                             @if($evaluationTrouvee) 
                                 {{$evaluationTrouvee->STATUSTYPE_LABEL}}
@@ -177,13 +172,10 @@ use App\Models\Skill;
                         @endphp
                     @endforeach
                     </td>
-                    @php $debug++; @endphp
-
                 @endforeach
                 </td>
                 @php
                     $i++;  
-                    $j++; 
                 @endphp 
             @endforeach
         </tbody>
